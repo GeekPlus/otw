9f093686efee8329d6db8703ad0dfdf05702be07
GeekPlus <geekplus.org@gmail.com>
2021-07-07 18:00:59 +0800

    Add files via upload

--- script/hdc.js
+++ script/hdc.js
@@ -7,13 +7,21 @@ function sign() {
     url: `https://hdchina.org`,
     headers: { Cookie: cookieVal }
   }
-  $httpClient.get(url, (error, response, data) => {
-      let title = `${cookieName}`
-      let subTitle = ``
-      let detail = `网站访问已完成`
-      console.log(`${title}, ${subTitle}, ${detail}`)
-      $notification.post(title, subTitle, detail)
-  })
+  if (!cookieVal) {
+    $notification.post("HDC", "", "未获取到cookie")
+  } else {
+    $httpClient.get(url, (error, response, data) => {
+      if (error) {
+        $notification.post("HDC", "", "访问出错")
+      } else {
+        let title = `${cookieName}`
+        let subTitle = ``
+        let detail = `网站访问已完成`
+        console.log(`${title}, ${subTitle}, ${detail}`)
+        $notification.post(title, subTitle, detail)
+      }
+    })
+  }
   $done({})
 }
 
