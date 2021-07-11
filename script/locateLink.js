37428544501c54e2fd1e5543cae877eefd205d34
GeekPlus <geekplus.org@gmail.com>
2021-07-11 14:34:40 +0800

    Add files via upload

--- script/locateLink.js
+++ script/locateLink.js
@@ -3,21 +3,10 @@ if (typeof $request !== "undefined") {
     const url = $request.url;
     const res =
         url.match(/geocode\/([0-9.]*)\/([0-9.]*)\//);
-    const latitude = res[1];
-    const longitude = res[2];
-
-    // write data
-    if (typeof $prefs !== 'undefined') {
-        // QX
-        $prefs.setValueForKey(latitude, "latitude");
-        $prefs.setValueForKey(longitude, "longitude");
-    } else {
-        // Loon & Surge
-        $persistentStore.write(latitude, "latitude");
-        $persistentStore.write(longitude, "longitude");
-    }
 
     if (res != null) {
+        const latitude = res[1];
+        const longitude = res[2];
         $.notify(
             "Current Location",
             "",
